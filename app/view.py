56b915782c2944a56f4edded523ac513201f0277
showwin <showwin_kmc@yahoo.co.jp>
2019-07-30 21:39:56 +0900

    [add] 一時保存

--- app/view.py
+++ app/view.py
@@ -7,6 +7,22 @@ view = Blueprint('root', __name__, url_prefix='/')
 def index():
     return render_template('index.html')
 
-@view.route('/hoge')
-def hoge():
-    return render_template('hoge.html')
+
+@view.route('/notification')
+def notification():
+    return render_template('notification.html')
+
+
+@view.route('/kanpo')
+def kanpo():
+    return render_template('kanpo.html')
+
+
+@view.route('/guidance')
+def guidance():
+    return render_template('guidance.html')
+
+
+@view.route('/list/hunin')
+def list_hunin():
+    return render_template('disease_list/hunin.html')
